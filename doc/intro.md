a9bdfa85738f6f2e68133865b5a64f25805783af
shofel <vistomin@examus.net>
2019-11-19 15:57:15 +0500

    git init.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj_digitsto100
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_digitsto100/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-digitsto100.core-test
+  (:require [clojure.test :refer :all]
+            [clj-digitsto100.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
