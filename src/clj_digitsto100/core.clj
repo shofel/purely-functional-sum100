3f9591452cdedfe14dbc523a68b55c728d8be7b2
shofel <vistomin@examus.net>
2019-11-19 16:01:57 +0500

    Link to the task.

--- src/clj_digitsto100/core.clj
+++ src/clj_digitsto100/core.clj
@@ -1,3 +1,15 @@
+;;;
+;;; @see https://purelyfunctional.tv/issues/purelyfunctional-tv-newsletter-352-tip-use-the-right-kind-of-comment-for-the-job/
+;;;
+;;; Write a program that outputs all possibilities to put a + or – or
+;;; nothing between the numbers 1-9 (in order) such that the result
+;;; is 100. For example:
+;;;
+;;; 1 + 2 + 34 - 5 + 67 - 8 + 9 = 100
+;;;
+;;; The function should output a list of strings with these formulas.
+;;;
+
 (ns clj-digitsto100.core)
 
 (defn foo
